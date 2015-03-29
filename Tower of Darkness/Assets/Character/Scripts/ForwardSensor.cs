1cb4c394929f8503321db758a97d3efc1c953756
Kevin Strileckis <KStrileckis@gmail.com>
2015-03-29 10:06:11 -0400

    Working with the controller

---
+++ Tower of Darkness/Assets/Character/Scripts/ForwardSensor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForwardSensor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	
+	void OnTriggerEnter2D(Collider2D other){
+		RaycastHit2D sensor;
+		Debug.Log("FUCK");
+		
+		if(other.CompareTag("Wall")){
+			sensor =  Physics2D.Raycast(transform.position, other.transform.position);
+			Debug.Log(sensor.distance);
+		}
+	}
+}
