f1a9395ceb2fed2149abaafc7586c13f70302f69
Igor Prokopenko <ip1990@knights.ucf.edu>
2015-03-31 20:00:28 -0400

    Committing the full directory with the right scripts
    Removed the whole standard folder and moves all the sprites and scripts.
    The backup folder in scripts is used only for reference, we can remove
    all the files there without affecting anything.
    
    -CharController now controls the character drag and drop the character
    on it.
    -SmoothFollow now controls the camera, it doesn't feel very smooth
    anymore we gotta look into it. Ill doublecheck with the older version,
    this may be due to larger camera size.

---
+++ Tower of Darkness/Assets/Scripts/Backup/CharControllerOrig.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharControllerOrig : MonoBehaviour {
+		
+		public float Speed = 0f;
+		public float MaxJumpTime = 2f;
+		public float JumpForce;
+		private float move = 0f;
+		private float JumpTime = 0f;
+		private bool CanJump;
+		
+		
+		void Start () {
+			JumpTime  = MaxJumpTime;
+		}
+		
+		
+		void Update ()
+		{
+			if (!CanJump)
+				JumpTime  -= Time.deltaTime;
+			if (JumpTime <= 0)
+			{
+				CanJump = true;
+				JumpTime  = MaxJumpTime;
+			}
+		}
+		
+		void FixedUpdate () {
+			move = Input.GetAxis ("Horizontal");
+			rigidbody2D.velocity = new Vector2 (move * Speed, rigidbody2D.velocity.y);
+			if (Input.GetKey (KeyCode.W)  && CanJump)
+			{
+				rigidbody2D.AddForce (new Vector2 (rigidbody2D.velocity.x,JumpForce));
+				CanJump = false;
+				JumpTime  = MaxJumpTime;
+			}
+		}
+		void OnTriggerEnter2D(Collider2D other) {
+			CanJump = true;
+		}
+
+}
