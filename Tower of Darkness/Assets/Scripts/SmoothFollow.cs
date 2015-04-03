702ccd23341d2cf8fb57699be3a8016332fb5edd
Igor Prokopenko <rus.igor3@gmail.com>
2015-04-03 15:33:39 -0400

    Cut the sprites and switched the camera script
    Used Mike's camera script from drive, cut the sprites in preparation to
    switching the characters and enemies.

--- Tower of Darkness/Assets/Scripts/SmoothFollow.cs
+++ Tower of Darkness/Assets/Scripts/SmoothFollow.cs
@@ -2,27 +2,63 @@
 using System.Collections;
 
 public class SmoothFollow : MonoBehaviour {
-	public Transform target;
-	public float distance = 10.0f;
-	public float height = 0f;
-	public float damping = 2.0f;
-	public bool smoothRotation = false;
-	public float rotationDamping = 10.0f;
-	public bool lockRotation = true;
 	
+	public Transform target;
+	public float damping = 0.3f;
+	public float lookAheadFactor = 0.5f;
+	public float lookAheadReturnSpeed = 0.5f;
+	public float lookAheadMoveThreshold = 0.1f;
+	public float yPosRestriction = -1;
+	
+	float offsetZ;
+	Vector3 lastTargetPosition;
+	Vector3 currentVelocity;
+	Vector3 lookAheadPos;
+	
+	float nextTimeToSearch = 0;
+	
+	// Use this for initialization
+	void Start () {
+		lastTargetPosition = target.position;
+		offsetZ = (transform.position - target.position).z;
+		transform.parent = null;
+	}
+	
+	// Update is called once per frame
 	void Update () {
-		Vector3 wantedPosition = target.TransformPoint(0, height, -distance);
-		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
 		
-		if (smoothRotation) {
-			Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
-			transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+		if (target == null) {
+			FindPlayer ();
+			return;
 		}
 		
-		else transform.LookAt (target, target.up);
-
-		if (lockRotation){
-			transform.localRotation = Quaternion.Euler(0,0,0);
+		// only update lookahead pos if accelerating or changed direction
+		float xMoveDelta = (target.position - lastTargetPosition).x;
+		
+		bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+		
+		if (updateLookAheadTarget) {
+			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+		} else {
+			lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);	
+		}
+		
+		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+		
+		newPos = new Vector3 (newPos.x, Mathf.Clamp (newPos.y, yPosRestriction, Mathf.Infinity), newPos.z);
+		
+		transform.position = newPos;
+		
+		lastTargetPosition = target.position;		
+	}
+	
+	void FindPlayer () {
+		if (nextTimeToSearch <= Time.time) {
+			GameObject searchResult = GameObject.FindGameObjectWithTag ("Player");
+			if (searchResult != null)
+				target = searchResult.transform;
+			nextTimeToSearch = Time.time + 0.5f;
 		}
 	}
 }
