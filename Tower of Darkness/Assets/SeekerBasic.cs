db75411385a11b482aa4ee2402c3c6107fff6790
Igor Prokopenko <ip1990@knights.ucf.edu>
2015-04-10 18:47:20 -0400

    My branch
    If you need the ghost to move just drag and drop the player onto the
    script for rigidbody. i dont even think you need to do that. you can
    modify my igorscene to make the player take dmg just add a script or
    modify my script

---
+++ Tower of Darkness/Assets/SeekerBasic.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeekerBasic : MonoBehaviour {
+
+
+	public float moveSpeed;
+	//public float turnSpeed;
+	private Vector3 moveDirection;
+	//public Rigidbody2D rigidbody2d;
+	private GameObject player;
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+		Debug.Log ("Player position: " + player.transform.position + ".");
+	}
+	
+
+	void Update () {
+		// 1
+		Vector3 currentPosition = transform.position;
+		// 2
+
+			// 3
+			//Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+			Vector3 moveToward = player.transform.position;
+			// 4
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize();
+
+		
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+		Debug.Log ("Current " + transform.position + ". Moving to " + target + ".");
+		//float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+		//transform.rotation = 
+			//Quaternion.Slerp( transform.rotation, 
+			               //  Quaternion.Euler( 0, 0, targetAngle ), 
+			                // turnSpeed * Time.deltaTime );
+	}
+	
+}
