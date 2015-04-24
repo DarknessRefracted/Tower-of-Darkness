274d37df524a062873f353af0e30125a08ef6650
Igor Prokopenko <rus.igor3@gmail.com>
2015-04-23 22:49:41 -0400

    Made the Nodes Kinematic with fixed angle
    Trying to check if the performance has improved

--- Tower of Darkness/Assets/Radar.cs
+++ Tower of Darkness/Assets/Radar.cs
@@ -27,7 +27,7 @@ public class Radar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log ("Contains " + nodeList.Count + " nodes.");
+		Debug.Log ("Contains " + nodeList.Count + " nodes.");
 
 		//Debug.Log ("Closest node is " + currentNode.position);
 
