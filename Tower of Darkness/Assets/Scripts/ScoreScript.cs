89133c791ee91a33d9e9163009f6516b30603ac1
msun03 <msun03@gmail.com>
2015-04-24 20:40:27 -0400

    Added Score script

---
+++ Tower of Darkness/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+	float playerScore = 0;
+	GUIStyle style = new GUIStyle();
+
+	void Update () 
+	{
+		playerScore += Time.deltaTime;
+	}
+	
+	public void IncreaseScore(int amount)
+	{
+		playerScore += amount;
+	}
+	
+	void OnDisable()
+	{
+		PlayerPrefs.SetInt("Score",(int)(playerScore*100));
+	}
+	
+	void OnGUI()
+	{
+		style.fontSize = 30;
+		style.font = (Font)Resources.Load("Something Strange");
+		GUI.Label (new Rect (55, 565, 100, 30), "Score: " + (int)(playerScore*100),style);
+	}
+}
