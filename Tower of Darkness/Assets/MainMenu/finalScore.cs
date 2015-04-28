5dfc77f3d5462282d692d2ecade583b9eedcd6be
Igor Prokopenko <ip1990@knights.ucf.edu>
2015-04-28 10:39:44 -0400

    This is the final product from class
    Has the added stuff from class like the changing background, next step
    is transferring it to Unity 5 before we continue working on it.

---
+++ Tower of Darkness/Assets/MainMenu/finalScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finalScore : MonoBehaviour {
+	
+	GUIStyle finalScoreStyle = new GUIStyle();
+	int score = ScoreScript.getScore();
+
+	void OnGUI(){
+		finalScoreStyle.fontSize = 60;
+		finalScoreStyle.normal.textColor = Color.red;
+		finalScoreStyle.font = (Font)Resources.Load("Something Strange");
+
+		GUI.Label (new Rect (Screen.width/2-180, Screen.height/2-80, 100, 50), "Final Score: " + score,finalScoreStyle);
+	}
+}
